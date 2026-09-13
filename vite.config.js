cb711315944ef5934edf1682ffa616fd01e3c48a
digiopsenvips <digiopsenvips@users.noreply.github.com>
2026-09-14 00:42:58 +0530

    Configure Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/project-astitva/',
+  base: process.env.VERCEL ? '/' : '/project-astitva/',
   plugins: [react()],
 })
